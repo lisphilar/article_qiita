42dc9b4b9b1c84a3d8d0a92305a649f5acaa62d0
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-08-30 19:07:35 +0900

    add: covsirphy-01

---
+++ covsirphy/01_sir/sir.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pprint import pprint
+import covsirphy as cs
+
+
+def main():
+    print(cs.__version__)
+    pprint(cs.SIR.EXAMPLE, compact=True)
+    # Reproduction number
+    eg_dict = cs.SIR.EXAMPLE.copy()
+    model_ins = cs.SIR(
+        population=eg_dict["population"],
+        **eg_dict["param_dict"]
+    )
+    print(model_ins.calc_r0())
+    # Set tau value and start date of records
+    example_data = cs.ExampleData(tau=1440, start_date="01Jan2020")
+    # Add records with SIR model
+    model = cs.SIR
+    area = {"country": "Full", "province": model.NAME}
+    example_data.add(model, **area)
+    # Records with model variables
+    df = example_data.specialized(model, **area)
+    # Plotting
+    cs.line_plot(
+        df.set_index("Date"),
+        title=f"Example data of {model.NAME} model",
+        y_integer=True,
+        filename="sir.png"
+    )
+
+
+if __name__ == "__main__":
+    main()
